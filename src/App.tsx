d297f3627a3315930e03c022b4d1706b1fd73952
imdevan <huapayadevan@gmail.com>
2025-12-01 19:05:19 -0800

    update browser router
    q

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.PROD ? "/get-dance-partner" : ""}>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/learn-more" element={<LearnMore />} />
